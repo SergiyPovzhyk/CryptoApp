d1488cb8b4120d8a96f479d0d60864d8011b0d30
Sergiy Povzhyk <sergiypovzhyk@gmail.com>
2023-06-12 18:20:20 +0300

    prepare domain layer

---
+++ app/src/main/java/com/example/cryptoapp/domain/usecases/GetCoinInfoUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.cryptoapp.domain.usecases
+
+import com.example.cryptoapp.domain.repository.CoinRepository
+
+class GetCoinInfoUseCase(private val repository: CoinRepository) {
+
+    operator fun invoke(fromSymbol:String) = repository.getCoinInfo(fromSymbol)
+}
