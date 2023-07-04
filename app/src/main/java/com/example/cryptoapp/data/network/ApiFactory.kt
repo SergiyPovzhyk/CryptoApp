8ed72b148dba5f3df1f70de429bd95f59e5bc9fd
Sergiy Povzhyk <sergiypovzhyk@gmail.com>
2023-07-04 09:08:36 +0300

    prepare data layer

---
+++ app/src/main/java/com/example/cryptoapp/data/network/ApiFactory.kt
@@ -0,0 +1,18 @@
+package com.example.cryptoapp.data.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiFactory {
+
+    private const val BASE_URL = "https://min-api.cryptocompare.com/data/"
+    const val BASE_IMAGE_URL = "https://cryptocompare.com"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val apiService = retrofit.create(ApiService::class.java)
+
+}
